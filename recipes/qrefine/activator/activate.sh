ba1c5c8471bb83526a05cbdd34c8424d6b44d517
Holger Kruse <mail2holger@gmail.com>
2023-11-23 14:39:48 +1100

    de/activator + git describe version

---
+++ recipes/qrefine/activator/activate.sh
@@ -0,0 +1,4 @@
+
+addons_path=`python -c 'import site; print(site.getsitepackages()[0])'`
+
+source $addons_path/build/setpaths.sh
---
+++ recipes/qrefine/activator/deactivate.sh
@@ -0,0 +1,3 @@
+
+addons_path=`python -c 'import site; print(site.getsitepackages()[0])'`
+source $addons_path/build/unsetpaths.sh
